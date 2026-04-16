ce9abbcffb104751a2e68b3a176557eaaabbad4c
Akshita Seth <sethakshita1331@gmail.com>
2026-04-16 23:13:16 +0000

    added comments

--- Arrays/TwoPointer/threeSum.hpp
+++ Arrays/TwoPointer/threeSum.hpp
@@ -27,7 +27,7 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue; //avoiding duplicates (substitutes using set data structure)
             int j=i+1;
             int k=n-1;
             while(j<k){
@@ -37,7 +37,7 @@ public:
                 else{
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++; k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[j]==nums[j-1]) j++;  //avoiding duplicates (substitutes using set data structure)
                     while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
