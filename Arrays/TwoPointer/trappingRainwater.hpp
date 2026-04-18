0ebcb22976490db1e1828e0ff148f9a792b9e262
Akshita Seth <sethakshita1331@gmail.com>
2026-04-18 06:11:17 +0000

    variation of problem

--- Arrays/TwoPointer/trappingRainwater.hpp
+++ Arrays/TwoPointer/trappingRainwater.hpp
@@ -3,6 +3,12 @@
 // Pattern: Two-pointer
 //https://takeuforward.org/data-structure/trapping-rainwater
 
+// What if width = n instead of 1?
+// If each block has width = n instead of 1, then every unit of trapped water spans n units horizontally.
+// That means the total trapped water volume is simply:
+
+// Total Water (with width n)=TotalWater (width=1)×𝑛
+
 // BFS: Taking current element i, callc maxLeft and maxRight each time with j loop, then calc total water: total += min(maxLeft, maxRight) - height[i];
 // TC: O(n^2)  SC:O(1)
 
