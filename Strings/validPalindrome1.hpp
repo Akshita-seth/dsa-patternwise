b9f82c94d1fb63664132992f3d238b3079d227ea
Akshita Seth <sethakshita1331@gmail.com>
2026-04-22 20:24:39 +0530

    Solved Valid Palindrome 1

---
+++ Strings/validPalindrome1.hpp
@@ -0,0 +1,26 @@
+// Problem: Valid Palindrome 1
+// Link: https://leetcode.com/problems/valid-palindrome/description/
+// Pattern: Two-pointer
+
+// Optimized: TC: O(N) SC: O(N)
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.size();
+        int i=0, j=n-1;
+        while(i<j)
+        {
+          //ignoring spaces and special characters
+         while(i<j && !isalnum(s[i])) i++;
+         while(i<j && !isalnum(s[j])) j--;
+           
+
+            if(tolower(s[i]) != tolower(s[j]))
+            return false;
+            
+            i++, j--;
+        }
+        return true;
+    }
+};
