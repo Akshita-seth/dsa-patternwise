7322864c498508e9c51348cb16bd76e693627384
Akshita Seth <sethakshita1331@gmail.com>
2026-05-01 19:14:12 +0530

    added bfs

--- Arrays/Sliding Window/maxConsecutive1s.hpp
+++ Arrays/Sliding Window/maxConsecutive1s.hpp
@@ -2,6 +2,28 @@
 // Link: https://leetcode.com/problems/max-consecutive-ones/
 // Pattern: Sliding Window
 
+//BFS: TC: O(N^2) 
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int count = 0;
+            for (int j = i; j < nums.size(); j++) {
+                if (nums[j] == 1) {
+                    count++;
+                    maxi = max(maxi, count);
+                } else {
+                    break; // stop when a 0 is hit
+                }
+            }
+        }
+        return maxi;
+    }
+};
+
+
 //Optimized Solution: TC: O(N) SC: O(1)
 
 class Solution {
