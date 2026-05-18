67f9fd9277e209f8acb81c7fdaecf03dd41c0057
Akshita Seth <sethakshita1331@gmail.com>
2026-05-18 23:58:06 +0530

    Add slidingWindowMax class for maximum in windows
    Implement sliding window maximum using a brute force approach.

---
+++ Arrays/Sliding Window/slidingWindowMax.hpp
@@ -0,0 +1,22 @@
+//Problem" Sliding Window Maximum
+//https://leetcode.com/problems/sliding-window-maximum/
+
+//BFS: TC: O(N^2)
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> ans;
+        for(int i=0; i<=n-k; i++)
+        {
+            int maxi=nums[i];
+            for(int j=i; j<i+k; j++)
+            {
+                maxi = max(maxi, nums[j]);
+            }
+            ans.push_back(maxi);
+        }
+        return ans;
+    }
+};
