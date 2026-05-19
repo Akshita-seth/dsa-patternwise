af949f5f831459894750dac1aaa7fea4e31134bd
Akshita Seth <sethakshita1331@gmail.com>
2026-05-19 18:59:07 +0530

    Optimize subarraysWithKDistinct using atMostK

--- Arrays/Sliding Window/subarraysWithKDistinct.hpp
+++ Arrays/Sliding Window/subarraysWithKDistinct.hpp
@@ -9,10 +9,9 @@ public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         int n=nums.size();
         int c=0;
-
+        unordered_map<int,int> freq;
         for(int i=0; i<n; i++)
-        {
-            unordered_map<int,int> freq; 
+        { 
             for(int j=i; j<n; j++)
             {
                 freq[nums[j]]++;
@@ -27,6 +26,36 @@ public:
 };
 
 
-//Optimized: 
-//
+//Optimized: TC: O(N) only r touches each index and worst case O(2N) since both la dn r will touch each indes i.e. if all digits distinct
+//Formulas used: 1. [exactlyK(nums,k) = atMostK(nums,k) - atMostK(nums,k-1)] 2. No. of subarrays ending at index r => r-l+1
+//atMostK(nums,k) => Gives no. of subarrays with <= k distinct digits
+//atMostK(nums,k-1) => Gives no. of subarrays with <= (k-1) distinct digits
+// We need no. of subarrays with exactly k distinct digits, hence, [exactlyK(nums,k) = atMostK(nums,k) - atMostK(nums,k-1)]
+
+class Solution {
+public:
+    int atMostK(vector<int>& nums, int k)
+    {
+        int n=nums.size();
+        int l=0,c=0 ;
+        unordered_map<int,int> freq;
+        for(int r=0; r<n; r++)
+        {
+            freq[nums[r]]++;   //EXPANDING
+    
+            while(freq.size() > k)  //SHRINKING
+            {
+                if(--freq[nums[l]] == 0)
+                freq.erase(nums[l]);
+                l++;
+            }
+            c += (r-l+1); //counting subarrays with distinct digits < & =k when tis is called atMostK(nums,k)
+        }
+        return c;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMostK(nums,k) - atMostK(nums,k-1);  
+    } 
+};
 
