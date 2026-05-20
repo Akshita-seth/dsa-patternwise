030eeef14888471ed9ac9185aeec9eb077dc2505
Akshita Seth <sethakshita1331@gmail.com>
2026-05-20 22:59:39 +0530

    Add reverseString function for string reversal

--- Strings/reverseString.hpp
+++ Strings/reverseString.hpp
@@ -17,3 +17,19 @@ public:
         }
     }
 };
+
+//without swap fn
+void reverseString(string &s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while (left < right) {
+        // Manual swap using a temp variable
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+}
