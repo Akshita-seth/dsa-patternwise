5ce3d43b87a329122bd36b9498da2853d4c6595c
Akshita Seth <sethakshita1331@gmail.com>
2026-05-28 17:03:40 +0530

    Refactor threeSum function with optimized approach
    Updated comments for space complexity and time complexity. Added a more optimized approach using hashset and improved the existing threeSum function.

--- Arrays/TwoPointer/threeSum.hpp
+++ Arrays/TwoPointer/threeSum.hpp
@@ -6,16 +6,78 @@
 
 //BFS: Three Nested Loops and set data structure to prevent duplicates
 // TC: O(n^3) * O(log(no. of triplets)) sice insertion in set is logN as it is unique   
-// SC: O(2*n) for set and result 
+// SC: O(2*(no. of triplets)) for set and result
+
+class Solution {
+public:
+    // Function to find triplets with sum zero
+    vector<vector<int>> threeSum(vector<int>& arr, int n) {
+        // Store unique triplets
+        set<vector<int>> st;
+
+        // First loop for first element
+        for (int i = 0; i < n; i++) {
+            // Second loop for second element
+            for (int j = i + 1; j < n; j++) {
+                // Third loop for third element
+                for (int k = j + 1; k < n; k++) {
+                    // If triplet sum is zero
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        // Store sorted triplet to avoid duplicates
+                        vector<int> temp = {arr[i], arr[j], arr[k]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+        // Convert set to vector
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
+
 
 // Better: Hashset use to find third number
 // TC: O(N2 * log(no. of unique triplets))
 // SC : O(2 * no. of the unique triplets) + O(N) as we are using a set data structure and
 // a list to store the triplets and extra O(N) for storing the array elements in another set.
 
+class Solution {
+public:
+    // Function to find triplets with sum zero
+    vector<vector<int>> threeSum(vector<int>& arr, int n) {
+        // Store unique triplets
+        set<vector<int>> ans;
+
+        // First loop for first element
+        for (int i = 0; i < n; i++) {
+            // Set to store elements seen in this iteration
+            set<int> hashset;
+
+            // Second loop for second element
+            for (int j = i + 1; j < n; j++) {
+                // Calculate third element needed
+                int third = -(arr[i] + arr[j]);
+
+                // If third already in set, we found a triplet
+                if (hashset.find(third) != hashset.end()) {
+                    vector<int> temp = {arr[i], arr[j], third};
+                    sort(temp.begin(), temp.end());
+                    ans.insert(temp);
+                }
+                // Add current element to set
+                hashset.insert(arr[j]);
+            }
+        }
+        // Convert set to vector
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
+
 
 // Optimized: 
-// TC: O(NlogN)+O(N2) sorting + TwoPointer * i loop
+// TC: O(NlogN)+O(N^2) sorting + TwoPointer * i loop, SC: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
