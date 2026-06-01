65dcee11680f67936fc0ede5ec31a1faeb699ea0
Akshita Seth <sethakshita1331@gmail.com>
2026-06-01 09:50:20 +0530

    Optimize maxArea function with two-pointer technique

--- Arrays/TwoPointer/containerWithMostWater.hpp
+++ Arrays/TwoPointer/containerWithMostWater.hpp
@@ -5,6 +5,24 @@
 // BFS: Nested loop, try all possible pairs
 // TC: O(n^2)  SC:O(1)
 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maxWater = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int width = j - i;
+                int water = width * min(height[i], height[j]);
+                maxWater = max(maxWater, water);
+            }
+        }
+        
+        return maxWater;
+    }
+};
+
 //Optimized: calc are by two pointers and move the pointer pointing the shorter line
 // TC: O(n) SC: O(1)
 
@@ -14,15 +32,18 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n= height.size();
-       int i=0, j=n-1; 
-       int waterMax = 0;
-       while(i<j)
-       {
-         waterMax = max(waterMax, (j-i)* min(height[i], height[j]));
-         if(height[i] < height[j]) i++;
-         else j--;
-       }
-       return waterMax;
+        int n=height.size();
+        int width, maxWater=0;
+        int left=0, right=n-1;
+        while(left < right)
+        {
+           width = right - left;
+           maxWater = max(maxWater, width* min(height[left], height[right]));
+           if(height[left] < height[right])
+           left++;
+           else
+           right--;
+        }
+        return maxWater;
     }
 };
